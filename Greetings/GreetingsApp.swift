ff1adabdf16b4045411c4ec6445d8adfa94e2c7a
Sameer Ankalagi <330.sameer@gmail.com>
2024-10-25 18:33:58 +0530

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Sameer Ankalagi on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
